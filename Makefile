e0009f6b5e11694b8b251e7e4f5ab7c948fd05ec
adiaz-uf <adiaz-uf@student.42madrid.com>
2025-11-19 17:43:11 +0100

    Fix: page theme

--- Makefile
+++ Makefile
@@ -1,16 +1,20 @@
 all:
+	@mkdir -p /home/adiaz-uf/data/mysql
+	@mkdir -p /home/adiaz-uf/data/wordpress
 	@docker compose -f ./srcs/docker-compose.yml up -d --build
 
 down:
 	@docker compose -f ./srcs/docker-compose.yml down
 
-clean:
-	@rm -rf /home/adiaz-uf/data/mysql/*
-	@rm -rf /home/adiaz-uf/data/wordpress/*
+fclean:
+	@sudo rm -rf /home/adiaz-uf/data/mysql/*
+	@sudo rm -rf /home/adiaz-uf/data/wordpress/*
 	@docker stop $$(docker ps -qa)
 	@docker rm $$(docker ps -qa)
 	@docker rmi $$(docker images -qa)
 	@docker volume rm $$(docker volume ls -q)
 	@docker network rm network
+
+re: fclean all
 
 .PHONY: all down clean
--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -2,17 +2,31 @@
 
 sleep 10
 
-if [ -f ./wp-config.php ]
-then
-	echo "wordpress already downloaded"
+# Check if WordPress is already installed by verifying if the database has tables
+if wp core is-installed --allow-root 2>/dev/null; then
+	echo "WordPress is already installed"
 else
-
-        wp core download --allow-root
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
+	echo "Installing WordPress..."
+	
+	# Download WordPress core if not present
+	if [ ! -f ./wp-config.php ]; then
+		wp core download --allow-root
+		wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
+	fi
+	
+	# Install WordPress
 	wp core install --allow-root --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIM --admin_password=$WORDPRESS_ADMIM_PASS  --admin_email=$WORDPRESS_ADMIM_EMAIL --skip-email
+	
+	# Update plugins
 	wp plugin update --path="." --allow-root --all
+	
+	# Create additional user
 	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASS --allow-root
-	wp theme install twentysixteen --activate --allow-root
+	
+	# Install and activate theme
+	wp theme install twentythree --activate --allow-root
+	
+	echo "WordPress installation completed"
 fi
 
 php-fpm7.4 -F
